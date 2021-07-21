fef45b1091e2021f1932f714b40cf75452d4a545
dani <danilopaz25@outlook.com>
2021-07-21 05:52:56 -0300

    added find command

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
 import os
 import discord
-from database import get_games,get_game_to,get_ranks_to,create_connection,get_mmr_title,insert_new_player,get_games_from_user_and_guild,get_ranks_from_user_and_guild
+from database import get_games,get_game_to,get_ranks_to,create_connection,get_mmr_title,insert_new_player,get_games_from_user_and_guild,get_ranks_from_user_and_guild,find_players
 from discord.ext import commands
 from games_config import games_with_icons,games_with_mmr
 
 intents = discord.Intents.all()
 
-token = "here goes the token"
+token = ""
 
 bot = commands.Bot(command_prefix='$',intents=intents)
 connector = None
@@ -16,20 +16,10 @@ try:
 except Exception as error:
     print('db not available {}'.format(error))
 
-@bot.command
-async def load(context,extension):
-    bot.load_extension('cogs.{}'.format(extension))
-    pass
-
-@bot.command
-async def unload(context,extension):
-    bot.unload_extension('cogs.{}'.format(extension))
-    pass
-
 def create_embed_with_title_from(a_title,a_dict_with_icons):
-    embed = discord.Embed(title=a_title,color=0xe67e22)
+    embed = discord.Embed(title=a_title.capitalize(),color=0xe67e22)
     for game,emoji in a_dict_with_icons.items():
-        embed.add_field(name = game, value= emoji, inline = True)
+        embed.add_field(name = game.capitalize(), value= emoji, inline = True)
     return embed
 
 def show_players_activity(a_guild):
@@ -85,6 +75,47 @@ async def clear(context):
     await context.channel.purge(limit=limit)
 
 @bot.command()
+async def find(context):
+    games_from_db = get_games(connector)
+    embed = create_embed_with_title_from('Select a game',games_from_db)
+    embed.set_author(name=context.author.name,icon_url=context.author.avatar_url)
+    game_selection = await context.author.send(embed=embed,delete_after=60)
+    for icon in games_from_db.values():
+        await game_selection.add_reaction(icon)
+
+    def check(reaction, user):
+        return not(user.bot) and user == context.author and reaction.message == game_selection and str(reaction.emoji) in games_from_db.values()
+
+    game_icon,player = await bot.wait_for('reaction_add',check=check)
+    game_title = get_game_to(connector,game_icon)
+    game_mmr = get_ranks_to(connector,game_title)
+
+    embed_mmr = create_embed_with_title_from(game_title,game_mmr)
+    embed_mmr.set_author(name="Now select a rank",icon_url=context.author.avatar_url)
+    mmr_selection = await context.author.send(embed=embed_mmr,delete_after=60)
+    for icon_mmmr in game_mmr.values():
+             await mmr_selection.add_reaction(icon_mmmr)
+
+    def check_mmr_selected(reaction, user):
+
+        if context.author == user and reaction.message == mmr_selection and not(user.bot):
+            if (str(reaction.emoji) in game_mmr.values()):
+                return (reaction.emoji,user)
+                  
+    game_mmr_icon, _ = await bot.wait_for('reaction_add',check=check_mmr_selected)
+    mmr_title = get_mmr_title(connector,game_mmr_icon)
+    players = find_players(connector,context.guild,game_title,mmr_title)
+    if players:
+        player_embed = discord.Embed(title='Found them',color=0xe67e22)
+        p = ' '
+        for name in players:
+            p += '**{}** '.format(name)
+        await context.channel.send(embed=player_embed)
+        await context.channel.send(p)
+    else:
+        await context.channel.send('Nothing found')
+
+@bot.command()
 async def add(context):
 
     games_from_db = get_games(connector)
@@ -104,7 +135,6 @@ async def add(context):
     embed_mmr = create_embed_with_title_from(game_title,game_mmr)
     embed_mmr.set_author(name="Now select your MMR",icon_url=context.author.avatar_url)
     mmr_selection = await context.author.send(embed=embed_mmr,delete_after=60)
-    await mmr_selection.add_reaction('<:X_:864075974110216193>')
     for icon_mmmr in game_mmr.values():
              await mmr_selection.add_reaction(icon_mmmr)
 
@@ -113,8 +143,6 @@ async def add(context):
         if context.author == user and reaction.message == mmr_selection and not(user.bot):
             if (str(reaction.emoji) in game_mmr.values()):
                 return (reaction.emoji,user)
-            elif str(reaction.emoji) == '<:X_:864075974110216193>':
-                return ('<:X_:864075974110216193>',user)
                   
     game_mmr_icon, _ = await bot.wait_for('reaction_add',check=check_mmr_selected)
     mmr_title = get_mmr_title(connector,game_mmr_icon)
